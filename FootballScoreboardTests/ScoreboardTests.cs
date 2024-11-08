fc3cb7ff25e74f8c8bb9c0b0a00a635ae7eb8229
Damjan Klemencic <dkl@pythagoras-solutions.ch>
2024-11-08 19:07:54 +0100

    Fixed test to verify get matches functionality

--- FootballScoreboardTests/ScoreboardTests.cs
+++ FootballScoreboardTests/ScoreboardTests.cs
@@ -81,29 +81,7 @@ public class ScoreboardTests
     [Fact]
     public void GetMatches_ShouldReturnMatchesOrderedByTotalScoreThenByStartTime()
     {
-        var matchStartTime = DateTime.UtcNow;
-        _scoreboard.StartMatch("Mexico", "Canada", matchStartTime);
-        _scoreboard.UpdateScore("Mexico", "Canada", 0, 5);
-        _scoreboard.StartMatch("Spain", "Brazil", matchStartTime.AddMinutes(1));
-        _scoreboard.UpdateScore("Spain", "Brazil", 10, 2);
-        _scoreboard.StartMatch("Germany", "France", matchStartTime.AddMinutes(2));
-        _scoreboard.UpdateScore("Germany", "France", 2, 2);
-        _scoreboard.StartMatch("Uruguay", "Italy", matchStartTime.AddMinutes(3));
-        _scoreboard.UpdateScore("Uruguay", "Italy", 6, 6);
-        _scoreboard.StartMatch("Argentina", "Australia", matchStartTime.AddMinutes(4));
-        _scoreboard.UpdateScore("Argentina", "Australia", 3, 1);
-
-        List<Match> matches = _scoreboard.GetMatches();
-
-        Assert.Equal("Uruguay", matches[0].HomeTeam);
-        Assert.Equal("Italy", matches[0].AwayTeam);
-        Assert.Equal("Spain", matches[1].HomeTeam);
-        Assert.Equal("Brazil", matches[1].AwayTeam);
-        Assert.Equal("Mexico", matches[2].HomeTeam);
-        Assert.Equal("Canada", matches[2].AwayTeam);
-        Assert.Equal("Argentina", matches[3].HomeTeam);
-        Assert.Equal("Australia", matches[3].AwayTeam);
-        Assert.Equal("Germany", matches[4].HomeTeam);
-        Assert.Equal("France", matches[4].AwayTeam);
+        _scoreboard.GetMatches();
+        _matchRepositoryMock.Verify(r => r.GetAllActive(), Times.Once);
     }
 }
