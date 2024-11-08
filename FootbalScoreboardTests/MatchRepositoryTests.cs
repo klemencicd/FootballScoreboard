609ae51131fc7312d747aaf375d523aa8c671a2d
Damjan Klemencic <dkl@pythagoras-solutions.ch>
2024-11-08 16:25:47 +0100

    Added test to verify getting a single match functionality

--- FootbalScoreboardTests/MatchRepositoryTests.cs
+++ FootbalScoreboardTests/MatchRepositoryTests.cs
@@ -22,4 +22,21 @@ public class MatchRepositoryTests
         Assert.Equal(0, matches[0].AwayTeamScore);
         Assert.Equal(matchStartTime, matches[0].StartTime);
     }
+
+    [Theory]
+    [InlineData("Mexico", "Canada")]
+    public void GetSingle_ShouldReturnCorrectMatch(string homeTeam, string awayTeam)
+    {
+        var matchStartTime = DateTime.UtcNow;
+        Match match = new(homeTeam, awayTeam, matchStartTime);
+        _matchRepository.Add(match);
+        Match? addeMatch = _matchRepository.GetSingle(homeTeam, awayTeam);
+
+        Assert.NotNull(addeMatch);
+        Assert.Equal(homeTeam, addeMatch.HomeTeam);
+        Assert.Equal(0, addeMatch.HomeTeamScore);
+        Assert.Equal(awayTeam, addeMatch.AwayTeam);
+        Assert.Equal(0, addeMatch.AwayTeamScore);
+        Assert.Equal(matchStartTime, addeMatch.StartTime);
+    }
 }
